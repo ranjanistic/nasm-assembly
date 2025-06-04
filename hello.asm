6735d1adf86f143bce9077ebab74ff8863e02325
ranjanistic <priyanshuranjan88@gmail.com>
2025-06-04 16:44:34 +0530

    init assembly hello-world

---
+++ hello.asm
@@ -0,0 +1,16 @@
+section .text
+    global _start
+
+_start:             ; linker entry point
+    mov edx,len    ; string length
+    mov ecx,msg     ; string
+    mov ebx,1       ; file descriptor
+    mov eax,4   ;  sys_write
+    int 0x80        ; call kernel
+
+    mov eax,1   ; sys_exit
+    int 0x80        ; call kernel
+
+section .data
+msg db 'Hello, World!', 0xA  ; string to print
+len equ $ - msg          ; length of the string
